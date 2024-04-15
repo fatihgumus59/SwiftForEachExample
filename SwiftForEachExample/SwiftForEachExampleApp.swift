a825f02bec5194d85cccb3ef496bd564c9a8528f
Fatih Gumus <fatihgumus72@gmail.com>
2024-04-15 14:24:06 +0300

    Initial Commit

---
+++ SwiftForEachExample/SwiftForEachExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftForEachExampleApp.swift
+//  SwiftForEachExample
+//
+//  Created by Fatih Gümüş on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftForEachExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
